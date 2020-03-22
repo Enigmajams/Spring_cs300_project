078ae74509f48ae3b087854bbaccd28ceaa6ba33
Enigmajams <onebookmore@gmail.com>
2020-03-22 16:09:52 -0400

    removed excess print

--- edu/cs300/ParallelTextSearch.java
+++ edu/cs300/ParallelTextSearch.java
@@ -65,7 +65,7 @@ public class ParallelTextSearch{
     
     for (int i = 0; i < numPassages; i++){     
     new Worker(passageArrays.get(i),i,workers[i],resultsOutputArray).start();
-    System.out.println("Created worker " + i + "\n");
+    //System.out.println("Created worker " + i + "\n");
     //new Worker(samples[1],1,workers[1],resultsOutputArray).start();
     }
     try {
