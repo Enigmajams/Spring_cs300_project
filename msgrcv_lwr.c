e2866c3fcb07b877b20b536581f882a5c580108f
Enigmajams <onebookmore@gmail.com>
2020-03-24 17:28:31 -0400

    trying 4 arguments

--- msgrcv_lwr.c
+++ msgrcv_lwr.c
@@ -28,7 +28,7 @@ int main(int argc, char**argv)
      //THIS IS WHERE MESSAGE GETS RECEIVED          
         
         fprintf(stderr, "msgget: msgget succeeded: msgqid = %d\n", msqid);
-        if (msgrcv(msqid, &pbuf, sizeof(pbuf.prefix)) == -1) {
+        if (msgrcv(msqid, &pbuf, sizeof(pbuf.prefix),0) == -1) {
          perror("msgrcv");
          return;
       }
