dc67ebdc7c26b6b059acee32e03c5150eed4f326
Enigmajams <onebookmore@gmail.com>
2020-03-24 17:55:12 -0400

    trying again

--- test.sh
+++ test.sh
@@ -7,6 +7,6 @@ gcc -shared -o libsystem5msg.so edu_cs300_MessageJNI.o -lc
 gcc -std=c99 -D_GNU_SOURCE msgsnd_pr.c -o msgsnd
 gcc -std=c99 -D_GNU_SOURCE msgrcv_lwr.c -o msgrcv
 ./msgsnd con
-java -cp . -Djava.library.path=. edu/cs300/ParallelTextSearch.java
+java -cp . -Djava.library.path=.  ParallelTextSearch.java
 ./msgrcv
 java edu.cs300.ParallelTextSearch con
