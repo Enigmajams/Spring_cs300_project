428fe5c9e1f9fe05f567c4a46aeef2f71b8bd7df
Enigmajams <onebookmore@gmail.com>
2020-03-24 17:31:13 -0400

    made small changes

--- msgrcv_lwr.c
+++ msgrcv_lwr.c
@@ -28,7 +28,7 @@ int main(int argc, char**argv)
      //THIS IS WHERE MESSAGE GETS RECEIVED          
         
         fprintf(stderr, "msgget: msgget succeeded: msgqid = %d\n", msqid);
-        if (msgrcv(msqid, &pbuf, sizeof(pbuf.prefix),0,0) == -1) {
+        if (msgrcv(msqid, &pbuf, sizeof(pbuf.prefix),0,0) < 0) {
          perror("msgrcv");
          return;
       }
