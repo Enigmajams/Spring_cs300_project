318213fa2fab1abc8e020f21aa61ea67c9afc8c0
Enigmajams <onebookmore@gmail.com>
2020-03-29 10:41:22 -0400

    alright first run through on this

--- edu/cs300/ParallelTextSearch.java
+++ edu/cs300/ParallelTextSearch.java
@@ -87,8 +87,10 @@ public class ParallelTextSearch{
         String results = (String)resultsOutputArray.take();
         System.out.println("results:"+results);
         counter++;
-         MessageJNI().writeLongestWordResponseMsg(0, prefix, counter, "/home/ecjackson5/Spring_cs300_project/"+fileList.get(counter), results, treeCount, 1)          
-          
+          if (results == "   ")
+              MessageJNI().writeLongestWordResponseMsg(0, prefix, counter, "/home/ecjackson5/Spring_cs300_project/"+fileList.get(counter), results, treeCount, 0)          
+          else 
+              MessageJNI().writeLongestWordResponseMsg(0, prefix, counter, "/home/ecjackson5/Spring_cs300_project/"+fileList.get(counter), results, treeCount, 1)          
       } catch (InterruptedException e) {};
     }
   }
