e2745de055cfaf0452700179c00fa926d82891b7
Enigmajams <onebookmore@gmail.com>
2020-03-29 16:42:03 -0400

    in theory message should work now

--- edu/cs300/ParallelTextSearch.java
+++ edu/cs300/ParallelTextSearch.java
@@ -88,9 +88,9 @@ public class ParallelTextSearch{
         System.out.println("results:"+results);
         counter++;
           if (results == "   ")
-          MessageJNI message =  new MessageJNI().writeLongestWordResponseMsg(0, prefix, counter, "/home/ecjackson5/Spring_cs300_project/"+fileList.get(counter), results, 5, 0);          
+          new MessageJNI().writeLongestWordResponseMsg(0, prefix, counter, "/home/ecjackson5/Spring_cs300_project/"+fileList.get(counter), results, 5, 0);          
           else 
-          MessageJNI message =  new MessageJNI().writeLongestWordResponseMsg(0, prefix, counter, "/home/ecjackson5/Spring_cs300_project/"+fileList.get(counter), results, 5, 1);          
+          new MessageJNI().writeLongestWordResponseMsg(0, prefix, counter, "/home/ecjackson5/Spring_cs300_project/"+fileList.get(counter), results, 5, 1);          
       } catch (InterruptedException e) {};
     }
   }
