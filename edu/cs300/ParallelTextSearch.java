fed8691741d60ea1a7a386df4f54eda9c9f4cdf7
Enigmajams <onebookmore@gmail.com>
2020-03-29 21:15:11 -0400

    first attempt at multiple prefix code

--- edu/cs300/ParallelTextSearch.java
+++ edu/cs300/ParallelTextSearch.java
@@ -3,105 +3,97 @@ import CtCILibrary.*;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.concurrent.*;
-import java.util.Scanner; 
+import java.util.Scanner;
 
 
 public class ParallelTextSearch{
-    
+
   public static void main(String[] args){
-           
-        SearchRequest test = new MessageJNI().readPrefixRequestMsg();
-        // System.out.println("message: " + test.prefix + " \n");
-        String prefix = test.prefix;
-      if (prefix.length() <= 2 ){
-        System.out.println("Provide prefix (min 3 characters) for search i.e. con\n");
-        System.exit(0);
-    }
-      
-      
-    try{ 
+    try{
         File passages = new File("/home/ecjackson5/Spring_cs300_project/passages.txt");
-        Scanner passageNameReader = new Scanner(passages); 
+        Scanner passageNameReader = new Scanner(passages);
         ArrayList<String> fileList = new ArrayList<String>();
         while(passageNameReader.hasNext()){
-            fileList.add(passageNameReader.next());         
+            fileList.add(passageNameReader.next());
         }
         int numPassages = fileList.size();
         //at this point filelist should contain all the file names
         ArrayList<String[]> passageArrays = new ArrayList<String[]>();
         for(int i=0;i<numPassages;i++){
-            
+
             File text = new File("/home/ecjackson5/Spring_cs300_project/"+fileList.get(i));
-            Scanner sc = new Scanner(text); 
+            Scanner sc = new Scanner(text);
             sc.useDelimiter("\\W|(?=\\S*['-])([a-zA-Z'-]+)|(\\b\\w{1,2}\\b)"); //checking regex to obtain the correct delimiter
 
             ArrayList<String> passageArrayList = new ArrayList<String>();
             while(sc.hasNext()){
-                passageArrayList.add(sc.next());         
-            }      
-      
+                passageArrayList.add(sc.next());
+            }
+
             String[] passageBasicArray = new String[passageArrayList.size()];
             passageBasicArray = passageArrayList.toArray(passageBasicArray);
             passageArrays.add(passageBasicArray);
         }
         //Theoretically the passageArrays arraylist now contains all the file strings
-        
-        /*File text = new File("/home/ecjackson5/Spring_cs300_project/Peter_Pan.txt");
-        Scanner sc = new Scanner(text); 
-        sc.useDelimiter("\\W|(?=\\S*['-])([a-zA-Z'-]+)"); //checking regex to obtain the correct delimiter
+        int treeCount = numPassages;
+        ArrayBlockingQueue[] workerQueues = new ArrayBlockingQueue[treeCount];
+        ArrayBlockingQueue resultsOutputArray = new ArrayBlockingQueue(treeCount*10);
 
-        ArrayList<String> list = new ArrayList<String>();
-        while(sc.hasNext()){
-            list.add(sc.next());         
-        }      
-      
-        String[] samples = new String[list.size()];
-        samples = list.toArray(samples);
-      */
-                  
-   int treeCount = numPassages;
-        
-    ArrayBlockingQueue[] workers = new ArrayBlockingQueue[treeCount];
-    ArrayBlockingQueue resultsOutputArray=new ArrayBlockingQueue(treeCount*10);
+        for (int i=0;i<treeCount;i++) {
+          workers[i] = new ArrayBlockingQueue(10);
+        }
+        ArrayList<Worker> workers = new ArrayList<Worker>()
+        for (int i = 0; i < numPassages; i++){
+          workers.add(new Worker(passageArrays.get(i),i,workers[i],resultsOutputArray).start());
+        }
 
-    
 
-     for (int i=0;i<treeCount;i++) {
-       workers[i]=new ArrayBlockingQueue(10);
-    }
-    
-    for (int i = 0; i < numPassages; i++){     
-    new Worker(passageArrays.get(i),i,workers[i],resultsOutputArray).start();
-    //System.out.println("Created worker " + i + "\n");
-    }
-    try {
-      for (int i = 0; i < numPassages; i++){  
-      workers[i].put(prefix);
+while(;;){
+        SearchRequest test = new MessageJNI().readPrefixRequestMsg();
+        String prefix = test.prefix;
+        if (prefix.length() <= 2 ){
+          System.out.println("Provide prefix (min 3 characters) for search i.e. con\n");
+          System.exit(0);
+          }
+        if (prefix = "   " ){
+          //Exit
+          workerQueues[i].put("exit");
+          for (Worker worker : workers) {
+              worker.join();
+          }
+          System.exit(0);
+        }
+
+
+        try {
+          for (int i = 0; i < numPassages; i++){
+          workerQueues[i].put(prefix);
+          }
+        } catch (InterruptedException e) {};
+
+
+        int counter=0;
+        while (counter<treeCount){
+          try {
+            String results = (String)resultsOutputArray.take();
+            Scanner resultScanner = new Scanner(results);
+                resultScanner.useDelimiter(":");
+            String longestWord = resultScanner.next();
+            int threadID = resultScanner.nextInt();
+            //System.out.println("results:"+results);
+            if (longestWord == "   ")
+              new MessageJNI().writeLongestWordResponseMsg(0, prefix, threadID, "/home/ecjackson5/Spring_cs300_project/"+fileList.get(threadID), longestWord, 5, 0);
+            else
+              new MessageJNI().writeLongestWordResponseMsg(0, prefix, threadID, "/home/ecjackson5/Spring_cs300_project/"+fileList.get(threadID), longestWord, 5, 1);
+            counter++;
+          } catch (InterruptedException e) {};
+        }
       }
-    } catch (InterruptedException e) {};
-      
-    int counter=0;
 
-    while (counter<treeCount){
-      try {
-        String results = (String)resultsOutputArray.take();
-        Scanner resultScanner = new Scanner(results); 
-            resultScanner.useDelimiter(":");
-        String longestWord = resultScanner.next();
-        int threadID = resultScanner.nextInt(); 
-        //System.out.println("results:"+results);
-        if (longestWord == "   ")
-          new MessageJNI().writeLongestWordResponseMsg(0, prefix, threadID, "/home/ecjackson5/Spring_cs300_project/"+fileList.get(threadID), longestWord, 5, 0);          
-        else 
-          new MessageJNI().writeLongestWordResponseMsg(0, prefix, threadID, "/home/ecjackson5/Spring_cs300_project/"+fileList.get(threadID), longestWord, 5, 1);          
-        counter++;
-      } catch (InterruptedException e) {};
-    }
-  }
-
-catch (IOException e) {
-    System.out.println("Error: File not found\n");
-      return;
+      catch (IOException e) {
+        System.out.println("Error: File not found\n");
+        return;
       }
+    }
   }
 }
