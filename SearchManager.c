4690725d29a9b3bd15276a5bba5546a68bfce4ef
Enigmajams <onebookmore@gmail.com>
2020-03-29 21:47:20 -0400

    updated to check for name

--- SearchManager.c
+++ SearchManager.c
@@ -86,10 +86,10 @@ int main(int argc, char**argv) //msgsnd
         } while ((ret < 0 ) && (errno == 4));
 
         if (rbuf.present == 1){
-            fprintf(stderr,"%ld, %d of %d, %s\n", rbuf.mtype, rbuf.index,rbuf.count,rbuf.longest_word);
+            fprintf(stderr,"%ld, %d of %d, %s\n", rbuf.passageName, rbuf.index,rbuf.count,rbuf.longest_word);
         }
         else{
-            fprintf(stderr,"%ld, %d of %d, not found\n", rbuf.mtype, rbuf.index,rbuf.count);
+            fprintf(stderr,"%ld, %d of %d, not found\n", rbuf.passageName, rbuf.index,rbuf.count);
         }
       }
 
