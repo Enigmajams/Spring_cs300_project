f41a40472bf0f27cc89bfbc694af26a9fafe252a
Enigmajams <onebookmore@gmail.com>
2020-03-30 16:41:46 -0400

    commented out success message not requested in req. doc

--- system5_msg.c
+++ system5_msg.c
@@ -188,7 +188,7 @@ JNIEXPORT void JNICALL Java_edu_cs300_MessageJNI_writeLongestWordResponseMsg
         exit(1);
     }
     else
-        fprintf(stderr,"Message: \"%d:%s\" Sent\n",rbuf.index, rbuf.longest_word);
+        //fprintf(stderr,"Message: \"%d:%s\" Sent\n",rbuf.index, rbuf.longest_word);
 
 
     (*env)->ReleaseStringUTFChars(env, prefixStr, prefix);
