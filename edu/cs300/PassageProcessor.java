b6bdf520cfbe4aae0193a2dfb9cfba9e398aaf49
Enigmajams <onebookmore@gmail.com>
2020-03-31 20:56:23 -0400

    changed class name

--- edu/cs300/PassageProcessor.java
+++ edu/cs300/PassageProcessor.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.concurrent.*;
 import java.util.Scanner;
 
-public class ParallelTextSearch{
+public class PassageProcessor{
   public static void main(String[] args){ 
         ArrayList<String> fileList = new ArrayList<String>(); 
         try{
